dfa8ab6bbe1d7019d33845d01e2cf6447e74f599
SUBIN PARK <psb8909@naver.com>
2023-01-20 01:39:45 +0900

    구글 로그인 연동하기

--- src/main/java/com/example/study/web/IndexController.java
+++ src/main/java/com/example/study/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.study.web;
 
+import com.example.study.config.auth.dto.SessionUser;
 import com.example.study.service.posts.PostsService;
 import com.example.study.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +9,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {  // Model- postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user=(SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
